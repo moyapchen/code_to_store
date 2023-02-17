d67b45403969a8a95c60b60fda34e7d6063a9484
Moya Chen <72097364+moyapchen@users.noreply.github.com>
2023-02-17 15:06:11 +0100

    Create tmux_session_attach_or_create.sh

---
+++ tmux_session_attach_or_create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SESSIONNAME=$1
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+then
+    tmux new-session -s $SESSIONNAME -n script -d
+#    tmux send-keys -t $SESSIONNAME "~/bin/script" C-m
+else
+    TAKEOVER_SESSION='temp_takeover_session'
+
+    if ! tmux has-session -t "$TAKEOVER_SESSION"; then
+        tmux new-session -d -s "$TAKEOVER_SESSION"
+        tmux set-option -t "$TAKEOVER_SESSION" set-remain-on-exit on
+        tmux new-window -kt "$TAKEOVER_SESSION":0  \
+            'echo "Use Prefix + L (^B L) to return to session."'
+    fi
+
+    # switch sessions to takeover
+    for client in $(tmux list-clients -t "$SESSIONNAME" | cut -f 1 -d :); do
+        tmux switch-client -c "$client" -t "$TAKEOVER_SESSION"
+    done
+fi
+
+tmux attach -t $SESSIONNAME
